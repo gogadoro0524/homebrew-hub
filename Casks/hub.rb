fb465c75b3001d3f20f9d5864c7b5739e2bf73e9
istd-gogadoro <istuza_ai@easeinv.com>
2026-08-28 20:04:15 +0900

    chore: release Hub 0.1.4

--- Casks/hub.rb
+++ Casks/hub.rb
@@ -1,6 +1,6 @@
 cask "hub" do
-  version "0.1.3"
-  sha256 "08a9ee815b2e8d7b801dc0c16bcb817a6b7d2cae66b03ab51fce31ac1e3efef1"
+  version "0.1.4"
+  sha256 "1bdc795f33fb5027331138c9e127014f314022fd20e1eaad6c75efaebf5e6df0"
 
   url "https://github.com/gogadoro0524/homebrew-hub/releases/download/v#{version}/Hub-#{version}-arm64.zip"
   name "Hub"

133d9ed2030cd187f180972d45773e14494129aa
istd-gogadoro <istuza_ai@easeinv.com>
2026-09-04 17:52:12 +0900

    bkgd 0.1.15

--- Casks/bkgd.rb
+++ Casks/bkgd.rb
@@ -1,6 +1,6 @@
 cask "bkgd" do
-  version "0.1.13"
-  sha256 "3e2c7679c07d9d05d25a50d1f3fd65a961b9c4329c31e8ab8cba4fefdbea97de"
+  version "0.1.15"
+  sha256 "a8be6290ea478cc72d31999a1f3b4ce162836c4aade2d80fb7043d0af9ab2322"
 
   url "https://github.com/gogadoro0524/homebrew-hub/releases/download/v#{version}/bkgd-#{version}-arm64.zip"
   name "bkgd"

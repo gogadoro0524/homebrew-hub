262e2da9d8b4365f7b809c4d2f26de743b47b9fd
istd-gogadoro <istuza_ai@easeinv.com>
2026-09-08 22:51:54 +0900

    worktory 0.1.28: collaboration release and Telegram recovery

--- Casks/worktory.rb
+++ Casks/worktory.rb
@@ -1,6 +1,6 @@
 cask "worktory" do
-  version "0.1.27"
-  sha256 "9727eda8a5a1c93cfad63fb2b374b2b0f164258b90863202e56d152c75f4ac20"
+  version "0.1.28"
+  sha256 "7601c99666092bdf125d1ee6031d43556378926ac02c2bb3c9c8ca5eb450801f"
 
   url "https://github.com/gogadoro0524/homebrew-hub/releases/download/v#{version}/worktory-#{version}-arm64.zip"
   name "worktory"

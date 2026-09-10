40b89e906f6256b2c5b06875850605cd5f0ef6c3
istd-gogadoro <istuza_ai@easeinv.com>
2026-09-10 20:34:39 +0900

    Update blueberry Desktop to 0.1.42

--- Casks/blueberry.rb
+++ Casks/blueberry.rb
@@ -1,6 +1,6 @@
 cask "blueberry" do
-  version "0.1.41"
-  sha256 "da8a1b524b81b3461182d8339ce9c0d03b30eaab4ed34559ef7daafdac9bfb39"
+  version "0.1.42"
+  sha256 "6b6bab7532e48a00619320f26c30a49cda012ef70783cd3dcf48ccc7f81df3cd"
 
   url "https://github.com/gogadoro0524/homebrew-hub/releases/download/v#{version}/blueberry-#{version}-arm64.zip"
   name "blueberry"

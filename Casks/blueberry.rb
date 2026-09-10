86bdac565af563fcf8a8c1b0ff09bb3b3a420dcc
gogadoro <133949049+gogadoro0524@users.noreply.github.com>
2026-09-11 02:05:56 +0900

    Update blueberry to 0.1.47

--- Casks/blueberry.rb
+++ Casks/blueberry.rb
@@ -1,6 +1,6 @@
 cask "blueberry" do
-  version "0.1.46"
-  sha256 "af3539f778d74e4f53db2b1a0b6ecedb2e65b3ec3ba390b520f1fd1ac1321ffa"
+  version "0.1.47"
+  sha256 "cedfaf324b81d858333ddcf67b6cfd6c884c9f95887b1a474e7ad791edde9682"
 
   url "https://github.com/gogadoro0524/homebrew-hub/releases/download/v#{version}/blueberry-#{version}-arm64.zip"
   name "blueberry"

7603f0b1c6df4fddfa80732763b0863d4b48b3d9
gogadoro <133949049+gogadoro0524@users.noreply.github.com>
2026-09-15 17:13:52 +0900

    blueberry 0.1.60

--- Casks/blueberry.rb
+++ Casks/blueberry.rb
@@ -1,6 +1,6 @@
 cask "blueberry" do
-  version "0.1.59"
-  sha256 "cc9046026a2422eec6afe732eba46eeef178a257283f53107939a87d31f092bf"
+  version "0.1.60"
+  sha256 "9ccb59cef50847637a9ab90cc133e747ebeb0861f963498250123e0c503daf94"
 
   url "https://github.com/gogadoro0524/homebrew-hub/releases/download/v#{version}/blueberry-#{version}-arm64.zip"
   name "blueberry"

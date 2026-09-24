4d4d6944fbb06d3de60a5cacadb4628ac2cec1c9
gogadoro0524 <gogadoro0524@gmail.com>
2026-09-24 17:50:39 +0900

    chore: update blueberry cask to 0.1.85

--- Casks/blueberry.rb
+++ Casks/blueberry.rb
@@ -1,6 +1,6 @@
 cask "blueberry" do
-  version "0.1.84"
-  sha256 "2399ec991ca5883421b4588f8ca384e491281aee0c37286c43a6bfcb0486145a"
+  version "0.1.85"
+  sha256 "f8930c4c79f4ae6064be886d47a7f9473175fc0408c4c16c16c98d2148600f78"
 
   url "https://github.com/gogadoro0524/homebrew-hub/releases/download/v#{version}/blueberry-#{version}-arm64.zip"
   name "blueberry"
